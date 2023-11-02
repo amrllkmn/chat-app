561a34cfdec2fabb745b041dc24e15af4eefed56
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2023-11-02 20:12:41 +0800

    Setup simple websocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    extract::ws::{WebSocket, WebSocketUpgrade},
+    response::Response,
+    routing::get,
+    Router,
+};
+
+use std::net::SocketAddr;
+
+async fn handle_socket(mut socket: WebSocket) {
+    while let Some(msg) = socket.recv().await {
+        let msg = if let Ok(msg) = msg {
+            msg
+        } else {
+            // client disconnected
+            return;
+        };
+
+        if socket.send(msg).await.is_err() {
+            // client disconnected
+            return;
+        };
+    }
+}
+
+async fn ws_handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(handle_socket)
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() {
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let app = Router::new()
+        .route("/ws", get(ws_handler))
+        .route("/", get(root));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+        .await
+        .unwrap();
 }
